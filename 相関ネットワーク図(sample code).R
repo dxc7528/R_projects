e875742ccc3c75b3cf2948896e26f80b8596bda7
dxc7528 <dxc7528@gmail.com>
2017-11-15 10:13:09 +0900

    use r do correlation network graph

--- 相関ネットワーク図(sample code).R
+++ 相関ネットワーク図(sample code).R
@@ -23,7 +23,7 @@ ggraph(graph_cors) +
   geom_edge_link(aes(edge_alpha = abs(r), edge_width = abs(r), color = r)) +
   guides(edge_alpha = "none", edge_width = "none") +
   scale_edge_colour_gradientn(limits = c(-1, 1), colors = c( "dodgerblue2","firebrick2")) +
-  geom_node_point(color = "white", size = 7) +
+  geom_node_point(color = "yellow", size = 7) +
   geom_node_text(aes(label = name), repel = TRUE) +
   theme_graph() +
   labs(title = "•]‰¿€–Ú‚Ì‘ŠŠÖ")
